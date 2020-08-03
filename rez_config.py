af7a5b0d4d171f115528c0d70a403ec6be406771
cundesi <cundesi.td@gmail.com>
2020-08-03 11:32:26 +0800

    update: config rez-pip

--- rez_config.py
+++ rez_config.py
@@ -1,3 +1,32 @@
-default_shell = "powershell"
+default_shell = "pwsh"
 set_prompt = False
 
+pip_install_remaps = [
+    # Typical bin copy behaviour
+    # Path in record          | pip installed to    | copy to rez destination
+    # ------------------------|---------------------|--------------------------
+    # ../../bin/*             | bin/*               | bin/*
+    {
+        "record_path": r"^{p}{s}{p}{s}(bin{s}.*)",
+        "pip_install": r"\1",
+        "rez_install": r"\1",
+    },
+    # Fix for https://github.com/nerdvegas/rez/issues/821
+    # Path in record          | pip installed to    | copy to rez destination
+    # ------------------------|---------------------|--------------------------
+    # ../../lib/python/*      | *                   | python/*
+    {
+        "record_path": r"^{p}{s}{p}{s}lib{s}python{s}(.*)",
+        "pip_install": r"\1",
+        "rez_install": r"python{s}\1",
+    },
+    # Path in record          | pip installed to    | copy to rez destination
+    # ------------------------|---------------------|--------------------------
+    # ../../lib/python/*      | *                   | python/*
+    {
+        "record_path": r"^{p}{s}{p}{s}(.*)",
+        "pip_install": r"\1",
+        "rez_install": r"\1",
+    },
+
+]
